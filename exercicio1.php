2d542c3c99621ee782374cee20f19853a3fa88b8
unknown <dennis.santana@frt.digital>
2023-08-09 17:30:00 -0300

    [ADD] resolução exercício 1

---
+++ exercicio1.php
@@ -0,0 +1,7 @@
+<?php
+
+	$location = ["Brasil" => "Brasília", "Alemanha" => "Berlim", "Chile" => "Santiago", "Equador" => "Quito", "Togo" => "Lomé"];
+	ksort($location);
+	echo "<pre>";
+	print_r($location);
+	echo "</pre>";
